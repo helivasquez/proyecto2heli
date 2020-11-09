774d898502412cd41521f94adfeb6a361e6915e3
helivasquez <70926227+helivasquez@users.noreply.github.com>
2020-11-08 23:54:31 -0600

    cambios para subir a heroku

--- main.py
+++ main.py
@@ -731,7 +731,7 @@ def conseguircomentario(n):
     return(respuesta)
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0",port = 3000,debug = True)
+    app.run( threaded = True ,host="0.0.0.0",port = 3000,debug = True)
 
 
 
